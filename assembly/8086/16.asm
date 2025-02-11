6f4d429fb4df25364d134fb7799e7c165bddeec3
WytheHuang <893609357@qq.com>
2025-02-11 23:30:28 +0800

    bi/ai register

---
+++ assembly/8086/16.asm
@@ -0,0 +1,22 @@
+assume cs:codesg,ds:datasg
+
+datasg segment
+    db 'welcome to masm!'
+    db '................'
+datasg ends
+
+codesg segment
+    start: mov ax, datasg
+           mov ds, ax
+           mov si, 0
+           mov cx, 8
+        s: mov ax, [si] 
+           mov [si+16], ax 
+           add si, 2
+           loop s
+
+           mov ax, 4c00h
+           int 21h
+codesg ends
+
+end start
---
+++ assembly/8086/17.asm
@@ -0,0 +1,27 @@
+assume cs:codesg, ds:datasg
+
+datasg segment
+  db '1. file         '
+  db '2. eidt         '
+  db '3. search       '
+  db '4. view         '
+  db '5. options      '
+  db '6. help         '
+datasg end
+
+codesg segment
+  start: mov ax,datasg
+         mov ds,ax
+         mov cx,6
+         mov bx,0
+      s: mov al, [bx+3]
+         and al,11011111b
+         mov [bx+3],al
+         add bx,16
+         loop s
+
+         mov ax, 4c00h
+         int 21h
+codesg ends
+
+end star
